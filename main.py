380c4dfc7ddca6e1ca9367348ba8b11ed85361ef
InsertShadowyHere <raphaelquarles@gmail.com>
2025-10-22 21:28:21 -0400

    first stable version!!! :)

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
 import os
-import tkinter as tk
 import sys
 import threading
-import datetime
-# from note import Note
 import subprocess
 from pystray import Icon, Menu, MenuItem
 from PIL import Image, ImageDraw
@@ -19,11 +15,17 @@ class HydraController:
         self.processes = []
 
     def create_note(self):
+        print("doing stuff")
+        thread = threading.Thread(target=self._create_note_process)
+        thread.start()
+
+    def _create_note_process(self):
+        lock = threading.Lock()
         result = subprocess.run([EXE_PATH, "creator.py"], capture_output=True, text=True)
         if result.returncode == 0 and result.stdout:
             proc = subprocess.Popen([EXE_PATH, "note.py", result.stdout])
-            self.processes.append(proc)
-
+            with lock:
+                self.processes.append(proc)
 
     def stop(self):
         for note in self.processes:
@@ -33,15 +35,16 @@ class HydraController:
                 pass
         os._exit(0)
 
+
 def create_image():
     image = Image.new("RGB", (64, 64), "white")
     draw = ImageDraw.Draw(image)
     draw.ellipse((8, 8, 56, 56), fill="red")
     return image
 
-# TODO: MAKE MENU ITEMS UPDATE WITH LISTS RUNNING
+# TODO: MAKE MENU ITEMS UPDATE WITH NOTE RUNNING
 icon = Icon("HydraNote", icon=Image.open("icon.png"), title="HydraNote",
-    menu=Menu(MenuItem("New Note", lambda: HydraController().create_note_process()),
+    menu=Menu(MenuItem("New Note", lambda: controller.create_note()),
          MenuItem("Quit", lambda: controller.stop())))
 
 def start_tray_icon():
--- note.py
+++ note.py
@@ -1,26 +1,50 @@
+import sys
+import time
 import tkinter as tk
+import json
+import random
+
 class Note:
     def __init__(self, message, end, freq=None):
-        self.message = message
-        self.freq = freq
-        if end == 0:
-            self.end = None
+        self.msg = message
+        if freq:
+            self.freq = freq
         else:
-            self.end = end
+            self.freq = end//6
+        if end == 0:
+            self.end = time.time()*2
+        else:
+            self.end = end + time.time()
         self.last_snoozed = None
+        self.total_snoozes = 0
+        self.est_snoozes = end // self.freq
 
+def popup():
+    root = tk.Tk()
+    root.title("HydraNote")
+    root.geometry("200x80+300+100")  # width x height + x + y
+    root.iconphoto(False, tk.PhotoImage(file="icon.png"))
+    label = tk.Label(root, text=note.msg)
+    label.pack(pady=10)
 
-root = tk.Tk()
-root.title("HydraNote")
-root.geometry("200x80+300+100")  # width x height + x + y
-root.iconphoto(False, tk.PhotoImage(file="icon.png"))
-label = tk.Label(root, text="Remember to take a break!")
-label.pack(pady=10)
+    button = tk.Button(root, text="Snooze", command=root.destroy)
+    button.pack()
 
-button = tk.Button(root, text="Snooze", command=root.destroy)
-button.pack()
+    root.mainloop()
 
-root.mainloop()
+# create Note information
+note_data = json.loads(sys.argv[1])
+note = Note(note_data["message"], end=note_data["time"])
+
+# Wait and popup
+while True:
+    note.last_snoozed = time.time()
+    time.sleep(note.freq)
+    popup()
+    note.total_snoozes += 1
+    if note.end and time.time() > note.end:
+        break
+
 """
 This file should handle each individual note's processing.
 """
